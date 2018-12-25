97d74dcc3bedd6b262a352c4f6d4dc09c215d610
Lam Nguyen <lnguyendev@gmail.com>
2018-12-24 23:07:20 -0500

    prepare for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const authRoute = require('./routes/api/auth');
 const usersRoute = require('./routes/api/users');
@@ -48,6 +49,15 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.json({ error: err });
 });
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
